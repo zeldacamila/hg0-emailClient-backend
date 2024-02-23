6e4489d7d5011d0789e3223c2ab51a2e1c43def7
DavedCV <97773641+DavedCV@users.noreply.github.com>
2024-02-22 22:16:27 -0500

    Add Email model

--- backend/email_api/models.py
+++ backend/email_api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from user_api.models import User
+
 # Create your models here.
+
+
+class Email(models.Model):
+    sender_email = models.EmailField()
+    recipient_email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    body = models.TextField()
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(default=False)
