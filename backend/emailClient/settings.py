659d4eb8cdaca8f7a2fa95a82b153eeadc35f6c8
emazou <estefania.mazou@gmail.com>
2024-02-24 02:58:14 -0500

    sad

--- backend/emailClient/settings.py
+++ backend/emailClient/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'django_extensions'
+    'django_extensions',
     'corsheaders',
     'drf_yasg',
     'user_api',
