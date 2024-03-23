510f1edcc9d607b0f2fd22dfa2d5fac0d47b0355
CamiloCJ09 <camilo.campaz1@u.icesi.edu.co>
2024-03-23 17:57:09 -0500

    Fix: Fixing config

--- backend/emailClient/settings.py
+++ backend/emailClient/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['*']
 if DEBUG:
     ALLOWED_HOSTS += ['localhost', '127.0.0.1']
 
